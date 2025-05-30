8b43dd5d4c611045504883876f6bd2f10b1af3a6
Sam Ramirez <samramirez.personal@gmail.com>
2025-05-30 23:37:15 +0500

    implement: labels

--- example.py
+++ example.py
@@ -2,11 +2,15 @@ from GitCommitChart import create_git_commit_chart
 import random
 
 if __name__ == "__main__":
-    # Example data array with 365 elements
-    # each with a random value between 0 and 100
-    data = [random.randint(0, 10) for i in range(365)] 
-
+    data = [random.randint(0, 10)
+            for i in range(365)]  # Example data for a year
 
     # Create the Git commit chart
-    image = create_git_commit_chart(data, rows_per_column=7)
+    image = create_git_commit_chart(
+        data,
+        rows_per_column=7,
+        horizontal_labels=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        vertical_labels=["Sun", "Wed", "Sat"],
+        label_font_size=40
+    )
     image.save("test.png")
